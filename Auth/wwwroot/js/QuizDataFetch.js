1d4142d1cf72b78ce900497c79659a17d1726ef9
MM <mariya.yushkova.1@uni.massey.ac.nz>
2021-09-06 16:38:48 +1200

    Added next button functions

--- Auth/wwwroot/js/QuizDataFetch.js
+++ Auth/wwwroot/js/QuizDataFetch.js
@@ -1,15 +1,11 @@
 ﻿
-   
-$(document).ready(function ()
-{
+function changeQuestionNumber() {
+    var qNumber = document.getElementById("changeQNumber").innerText;
+    var change = parseInt(qNumber, 10) + 1;
+    document.getElementById("changeQNumber").innerHTML = change.toString();
+}
 
-    //this.state = {
-    //        birdData: [],
-    //        habitatsData: [],
-    //        questionData: [],
-    //        orderData: [],
-    //};
-
+function dropDownOptions() {
     $.get("https://localhost:44358/birdquiz", function (birds, status) {
         // get select (dropdown box)
         var selectBox = $("#12345");
@@ -22,21 +18,28 @@ $(document).ready(function ()
             }));
         });
     });
+}
 
+function getNextQuestion() {
     $.get("https://localhost:44358/getQuestion", function (bird, status) {
-
         // Set the source of the music element DYNAMICALLY so that it gets the sound based on the 'bird._id'
-        // I.e.  
-        $("#idOfMusicElement").attr("src", "https://localhost:44358/getSound/" + bird._id);
-        
+        var audio = document.getElementById("idOfMusicElement");
+        audio.src = "https://localhost:44358/getSound/" + bird._id;
+        audio.load();
+
+        $("#answer").text(bird.name);
+        changeQuestionNumber();
     });
 
     //DO NOT DELETE BELOW CODE: Example of how to link to a button click function
     //$("#hellobtn").click(function () {        
     //});
-
+}
    
-
+$(document).ready(function ()
+{
+    dropDownOptions();
+    getNextQuestion();
 });
 
 
